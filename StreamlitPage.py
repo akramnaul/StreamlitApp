21f6d5327e4a554dde2ca54cda05ac87e3944a6d
akramnaul <60735293+akramnaul@users.noreply.github.com>
2023-05-14 22:55:14 +0500

    Update StreamlitPage.py

--- StreamlitPage.py
+++ StreamlitPage.py
@@ -5,26 +5,8 @@ st.header("StreamlitPage Header")
 st.title("StreamlitPage Title")
 st.write("Hello StreamlitPage !")
 
-# Draw a title and some text to the app:
-'''
-# This is the document title
-
-This is some _markdown_.
-'''
-
-import pandas as pd
-df = pd.DataFrame({'col1': [1,2,3]})
-df  # 👈 Draw the dataframe
-
-x = 10
-'x', x  # 👈 Draw the string 'x' and then the value of x
-
-# Also works with most supported chart types
-import matplotlib.pyplot as plt
-import numpy as np
-
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-fig  # 👈 Draw a Matplotlib chart
+st.subheader('This is a subheader')
+st.markdown('_Markdown_')
+st.text('This is sample text')
+st.latex(r''' e^{i\pi} + 1 = 10 ''')
+st.caption('This is a caption')
