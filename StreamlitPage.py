46023edb6bc8f43c4c651573e58e612cd688506d
akramnaul <60735293+akramnaul@users.noreply.github.com>
2023-05-14 22:32:17 +0500

    Update StreamlitPage.py

--- StreamlitPage.py
+++ StreamlitPage.py
@@ -1,7 +1,12 @@
 import streamlit as st
+from PIL import Image
 
 st.header("StreamlitPage Header")
-
 st.title("StreamlitPage Title")
-
 st.write("Hello StreamlitPage !")
+
+# opening the image
+image = Image.open('A.jpg')
+
+#displaying the image on streamlit app
+st.image(image, caption='Enter any caption here')
