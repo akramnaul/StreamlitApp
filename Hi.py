4a2573f37d774bdab1e851ff8555ba5662fd4f53
akramnaul <60735293+akramnaul@users.noreply.github.com>
2023-05-17 01:34:45 +0500

    Update Hi.py

--- Hi.py
+++ Hi.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import subprocess
 
 code = '''def hello():
     print("Hello, Streamlit!")'''
@@ -10,3 +11,5 @@ def hello(name):
 first_variable = st.text_input("What's your name ? ")
 if first_variable:
     st.write(hello(first_variable))
+
+subprocess.run([f"{sys.executable} ", "-m scoop", "StreamlitPage.py"])
