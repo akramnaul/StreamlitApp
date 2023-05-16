b572a768e4a19e87b894e630335632fea000c8b9
akramnaul <60735293+akramnaul@users.noreply.github.com>
2023-05-17 01:18:42 +0500

    Update Hi.py

--- Hi.py
+++ Hi.py
@@ -4,3 +4,9 @@ code = '''def hello():
     print("Hello, Streamlit!")'''
 
 st.code(code, language='python')
+
+def hello(name):
+    return 'Hello ' + name
+first_variable = st.text_input('What's your name?')
+if first_variable:
+    st.write(hello(first_variable))
